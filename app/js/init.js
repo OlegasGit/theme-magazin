2014cbe87b4e7e1c3103db1a7d85bdc189d41379
OlegasGit <olegasbox@gmail.com>
2018-04-25 16:15:36 +0300

    Фух. Карусель на месте, собираю майн

--- app/js/init.js
+++ app/js/init.js
@@ -1,5 +1,20 @@
+// init carousel
 $('.main-slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  prevArrow: false,
+  nextArrow: false,
+});
+
+if(window.matchMedia('(max-width: 991px)').matches)
+{
+    $('.main-slider').slick({
+      slidesToShow: 3,
+      autoplay: false,
+    });
+}
+
+	
