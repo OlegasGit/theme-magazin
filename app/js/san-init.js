6c88a757608c85812d1230c3d3d8003a44da3c08
Oksen Oleksandr <aledor777@gmail.com>
2018-04-26 18:38:27 +0300

    Восстановил git

---
+++ app/js/san-init.js
@@ -0,0 +1,24 @@
+$( function() {
+	// Init tabs
+    $( "#tabs" ).tabs();
+    $( "#tabs-product" ).tabs();
+  } );
+
+
+
+$('.medium-card').on('click', function(){
+    // Find cards and remove class row-card
+    $('.all-items-container .card').removeClass('row-card');
+    $('.all-items-container .col-4').removeClass('col-12');
+    $('.all-items-container .card-img').removeClass('d-none');
+    $('.all-items-container .row-card-img').removeClass('d-block');
+});
+
+$('.row-card').on('click', function(){
+    // Find cards and add class row-card
+    $('.all-items-container .card').addClass('row-card');
+    $('.all-items-container .col-4').addClass('col-12');
+    $('.all-items-container .card-img').addClass('d-none');
+    $('.all-items-container .row-card-img').addClass('d-block');
+});
+
