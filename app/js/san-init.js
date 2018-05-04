c039dd414d8c8c1965cfdeae6c3fd2eb2918edc0
OlegasGit <olegasbox@gmail.com>
2018-05-04 11:09:16 +0300

    адаптив

--- app/js/san-init.js
+++ app/js/san-init.js
@@ -1,23 +1,3 @@
-// init carousel
-$('.main-slider').slick({
-  slidesToShow: 4,
-  slidesToScroll: 2,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  dots: true,
-  prevArrow: false,
-  nextArrow: false,
-});
-
-if(window.matchMedia('(max-width: 991px)').matches)
-{
-    $('.main-slider').slick({
-      slidesToShow: 3,
-      autoplay: false,
-    });
-}
-
-// init Aleksander
 $( function() {
 	// Init tabs
     $( "#tabs" ).tabs();
@@ -42,5 +22,3 @@ $('.row-card').on('click', function(){
     $('.all-items-container .row-card-img').addClass('d-block');
 });
 
-// init Inna
-	
